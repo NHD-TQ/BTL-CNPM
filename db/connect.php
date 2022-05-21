82423f3ac6246c15ac1990ba7f3e24d23fbaa059
khanhtran21032001 <99745307+khanhtran21032001@users.noreply.github.com>
2022-05-21 16:15:02 +0700

    xoa file ko can thiet

--- db/connect.php
+++ db/connect.php
@@ -7,7 +7,7 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 	  // Change character set to utf8
-	mysqli_set_charset($con,"utf8");
+	mysqli_set_charset($con,"utf8")      ;
 
 	
 ?>
